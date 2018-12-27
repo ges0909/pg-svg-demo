42d83afff394b6468a4e0c8822467ab6b8c7e4e1
Gerrit Schrader <gerrit.schrader@gmail.com>
2018-12-27 18:14:14 +0100

    service impl. improved

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <svg viewBox="0 0 100 100" preserveAspectRatio="none">
-    <Service x="10" y="10" width="20" height="20" fill="yellow" label="Consolidator" />
-    <Service x="10" y="50" width="20" height="20" fill="lightblue" label="Normality Generator" />
-    <Service x="30" y="30" width="20" height="20" fill="lightblue" label="Recorder of Anomalies" />
-    <Service x="50" y="10" width="20" height="20" fill="lightblue" label="Predictor" />
-    <Service x="50" y="50" width="20" height="20" fill="lightblue" label="Alarm Generator" />
+  <svg viewBox="0 0 100 100"> <!-- preserveAspectRatio="align" -->
+    <Service x="10" y="10" label="Consolidator" />
+    <Service x="10" y="50" label="Normality Generator" />
+    <Service x="30" y="30" label="Recorder of Anomalies" />
+    <Service x="50" y="10" label="Predictor" />
+    <Service x="50" y="50" label="Alarm Generator" />
   </svg>
 </template>
 
--- src/components/Service.vue
+++ src/components/Service.vue
@@ -1,31 +1,62 @@
 <template>
-  <svg :x="x" :y="y" :width="width" :height="height">
-    <rect :width="width" :height="height" :fill="fill" rx="10" ry="10" />
-    <text x="50%" y="50%" class="text" text-anchor="middle" alignment-baseline="middle" v-html="multilineLabel"></text>
+  <svg :x="x" :y="y" :width="boxWidth" :height="boxHeight" :viewBox="viewBox">
+    <rect :x="rectX" :y="rectY" :width="rectWidth" :height="rectHeight" :rx="rectRxy" :ry="rectRxy" :stroke-width="strokeWidth" vector-effect="non-scaling-stroke" />
+    <text x="50%" y="50%" text-anchor="middle" alignment-baseline="middle" v-html="multilineLabel" />
   </svg>
 </template>
 
 <script>
 export default {
-  props: ["x", "y", "width", "height", "fill", "label"],
-  data: () => ({}),
+  props: ["x", "y", "label"],
+  data: () => ({
+    boxWidth: 30,
+    boxHeight: 20,
+    rectRxy: 15,
+    strokeWidth: 1
+  }),
   computed: {
+    viewBox() {
+      return `0 0 ${this.boxWidth} ${this.boxHeight}`;
+    },
+    rectX() {
+      return this.strokeWidth;
+    },
+    rectY() {
+      return this.strokeWidth;
+    },
+    rectWidth() {
+      return parseInt(this.boxWidth) - 2 * this.strokeWidth;
+    },
+    rectHeight() {
+      return parseInt(this.boxHeight) - 2 * this.strokeWidth;
+    },
     multilineLabel() {
       let words = this.label.split(/\s+/);
-      let dy = (0.5 /*horizontal center*/ - (words.length - 1) * 1.2) / 2;
+      let dy = (0.5 - (words.length - 1) * 1.2) / 2;
+      // debugger
       return words
-        .map((word, index) => `<tspan x="50%" dy="${index == 0 ? dy : 1.2}em">${word}</tspan>`)
+        .map(
+          (word, index) =>
+            `<tspan x="50%" dy="${index == 0 ? dy : 1.2}em">${word}</tspan>`
+        )
         .join("");
     },
     translate() {
-      return `translate(${this.x} ${this.y})`; // back ticks = string template
+      return `translate(${this.x} ${this.y})`;
     }
   }
 };
 </script>
 
 <style scoped>
-.text {
+rect {
+  fill: tomato;
+  opacity: 0.7;
+  stroke: black;
+  stroke-opacity: 0.9;
+}
+text {
   font-size: 3px;
+  font-weight: bolder;
 }
 </style>
